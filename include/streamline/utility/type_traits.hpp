a14a297be06f1ef2a1eaffd32b6d781e76f06e49
Rob Clucas <robjclucas@gmail.com>
2019-10-23 18:39:01 +0100

    Add variadic and conversion type traits
    This adds a type trait which wraps c++17 std::is_convertible so that it
    can be used with c++14. It also adds a trait which enables a function if
    the size of a variadic parameter pack matches a compile time specified
    size.

--- include/streamline/utility/type_traits.hpp
+++ include/streamline/utility/type_traits.hpp
@@ -29,6 +29,13 @@ template <typename Base, typename Impl>
 static constexpr bool is_base_of_v = 
   std::is_base_of<std::decay_t<Base>, std::decay_t<Impl>>::value;
 
+/// Wrapper around std::is_convertible for c++14 compatibility.
+/// \tparam From The type to convert from.
+/// \tparam To   The type to convert to.
+template <typename From, typename To>
+static constexpr bool is_convertible_v =
+  std::is_convertible<std::decay<From>, std::decay_t<To>>::value;
+
 /// Wrapper around std::is_same for c++-14 compatibility.
 /// \tparam T1 The first type for the comparison.
 /// \tparam T2 The second type for the comparison.
@@ -41,6 +48,14 @@ static constexpr bool is_same_v =
 //==--- [streamline traits] ------------------------------------------------==//
 
 namespace streamline {
+
+/// Defines a valid type when the number of elements in the variadic pack
+/// matches the size defined by Size.
+/// \tparam Size   The size that the pack must be.
+/// \tparam Values The values in the pack.
+template <std::size_t Size, typename... Values>
+using variadic_size_enable_t =
+  std::enable_if_t<Size == sizeof...(Values), int>;
 
 /// Defines a valid type if Size is less than `streamline_max_unroll_depth`.
 /// \tparam Size The size of the unrolling.
