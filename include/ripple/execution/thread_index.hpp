1db3972e39b8e255b2ccbaa17a051d512b8ec210
Rob Clucas <robjclucas@gmail.com>
2019-12-05 08:50:10 +0000

    Add normalised block & global index functionality

--- include/ripple/execution/thread_index.hpp
+++ include/ripple/execution/thread_index.hpp
@@ -20,6 +20,38 @@
 
 namespace ripple {
 
+//==--- [sizes] ------------------------------------------------------------==//
+
+/// Returns the value of the block size in the grid in a given dimension. The
+/// dimension must be one of dim_x, dim_y, dim_z, or a value specifting the
+/// index of the dimension.
+/// \param  dim the dimension to get the block size for.
+/// \tparam Dim the type of the dimension specifier.
+template <typename Dim>
+ripple_host_device inline auto block_size(Dim&& dim) -> std::size_t {
+  return detail::block_size(std::forward<Dim>(dim));
+}
+
+/// Returns the value of the grid size in the grid (number of blocks in the
+/// grid) in a given dimension. The dimension must be one of dim_x, dim_y,
+/// dim_z, or a value specifting the index of the dimension.
+/// \param  dim the dimension to get the block size for.
+/// \tparam Dim the type of the dimension specifier.
+template <typename Dim>
+ripple_host_device inline auto grid_size(Dim&& dim) -> std::size_t {
+  return detail::grid_size(std::forward<Dim>(dim));
+}
+
+/// Returns the value of the global size of the grid (number of threads in the
+/// grid) in a given dimension. The dimension must be one of dim_x, dim_y,
+/// dim_z, or a value specifting the index of the dimension.
+/// \param  dim the dimension to get the global size for.
+/// \tparam Dim the type of the dimension specifier.
+template <typename Dim>
+ripple_host_device inline auto global_size(Dim&& dim) -> std::size_t {
+  return detail::grid_size(dim) * detail::block_size(dim);
+}
+
 //==--- [indexing] ---------------------------------------------------------==//
 
 /// Returns the value of the thread index in the block in a given dimension. The
@@ -52,37 +84,58 @@ ripple_host_device inline auto global_idx(Dim&& dim) -> std::size_t {
   return detail::global_idx(std::forward<Dim>(dim));
 }
 
-//==--- [sizes] ------------------------------------------------------------==//
-
-/// Returns the value of the block size in the grid in a given dimension. The
-/// dimension must be one of dim_x, dim_y, dim_z, or a value specifting the
-/// index of the dimension.
-/// \param  dim the dimension to get the block size for.
-/// \tparam Dim the type of the dimension specifier.
+/// Returns the normalized global index in the \p dim dimension, using the
+/// global number of threads as the dimension size.
+/// \param  dim The dimension to get the normalized index for.
+/// \tparam Dim The type of the dimension specifier.
 template <typename Dim>
-ripple_host_device inline auto block_size(Dim&& dim) -> std::size_t {
-  return detail::block_size(std::forward<Dim>(dim));
+ripple_host_device inline auto global_norm_idx(Dim&& dim) -> float {
+  return static_cast<float>(global_idx(dim)) 
+    / static_cast<float>(grid_size(dim));
 }
 
-/// Returns the value of the grid size in the grid (number of blocks in the
-/// grid) in a given dimension. The dimension must be one of dim_x, dim_y,
-/// dim_z, or a value specifting the index of the dimension.
-/// \param  dim the dimension to get the block size for.
-/// \tparam Dim the type of the dimension specifier.
+/// Returns the normalized block index in the \p dim dimension, using the number
+/// of threads in the block as the block size.
+/// \param  dim The dimension to get the normalized index for.
+/// \tparam Dim The type of the dimension specifier.
 template <typename Dim>
-ripple_host_device inline auto grid_size(Dim&& dim) -> std::size_t {
-  return detail::grid_size(std::forward<Dim>(dim));
+ripple_host_device inline auto block_norm_idx(Dim&& dim) -> float {
+  return static_cast<float>(block_idx(dim)) 
+    / static_cast<float>(block_size(dim));
 }
 
-/// Returns the value of the global size of the grid (number of threads in the
-/// grid) in a given dimension. The dimension must be one of dim_x, dim_y,
-/// dim_z, or a value specifting the index of the dimension.
-/// \param  dim the dimension to get the global size for.
-/// \tparam Dim the type of the dimension specifier.
+/// Returns the normalized global index in the \p dim dimension, using the \p
+/// dim_size as the number of elements in the dimension.
+///
+/// This overload is useful for the case that there may be more threads running
+/// than elements in the dimension.
+///
+/// \param  dim      The dimension to get the normalized index for.
+/// \param  dim_size The number of elements in the dimension.
+/// \tparam Dim      The type of the dimension specifier.
 template <typename Dim>
-ripple_host_device inline auto global_size(Dim&& dim) -> std::size_t {
-  return detail::grid_size(dim) * detail::block_size(dim);
+ripple_host_device inline auto global_norm_idx(Dim&& dim, std::size_t dim_size)
+-> float {
+  return static_cast<float>(global_idx(dim)) / static_cast<float>(dim_size);
 }
+
+/// Returns the normalized block index in the \p dim dimension, using the \p
+/// dim_size as the number of elements in the dimension.
+///
+/// This overload is useful for the case that some threads in a block do not
+/// run.
+///
+/// \param  dim      The dimension to get the normalized index for.
+/// \param  dim_size The number of elements in the dimension.
+/// \tparam Dim      The type of the dimension specifier.
+template <typename Dim>
+ripple_host_device inline auto block_norm_idx(Dim&& dim, std::size_t dim_size)
+-> float {
+  return static_cast<float>(block_idx(dim)) / static_cast<float>(dim_size);
+}
+
+
+//==--- [utilities] --------------------------------------------------------==//
 
 /// Returns true if the thread is the first thread in the block for dimension
 /// \p dim.
