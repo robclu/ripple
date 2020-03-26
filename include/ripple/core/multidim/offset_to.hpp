015536ad766a6bc1f0f71267b22e5e528949dc53
Rob Clucas <robjclucas@gmail.com>
2020-03-26 09:38:29 +0000

    Update offset computations for storage

--- include/ripple/core/multidim/offset_to.hpp
+++ include/ripple/core/multidim/offset_to.hpp
@@ -30,18 +30,19 @@ namespace ripple {
 ///
 /// For example, for a 2D velocity array with u and v, the layout would be
 ///
-/// ~~~
-/// index:    0   1   2   3   ...   n
-/// ---     ===========================
-///         | u | u | u | u | ... | u |
-///  0      ---------------------------
-///         | v | v | v | v | ... | v |
-/// ---     ===========================
-///         | u | u | u | u | ... | u |
-///  1      ---------------------------
-///         | v | v | v | v | ... | v |
-/// ---     ===========================
-/// ~~~
+/// <pre class="diagram">
+/// +--------+---+---+---+---+-----+---+
+/// | index: | 0 | 1 | 2 | 3 | ... | n |
+/// +--------+---+---+---+---+-----+---+
+/// |        | u | u | u | u | ... | u |
+/// +   0    +---+---+---+---+-----+---+
+/// |        | v | v | v | v | ... | v |
+/// +--------+---+---+---+---+-----+---+
+/// |        | u | u | u | u | ... | u |
+/// +   1    +---+---+---+---+-----+---+
+/// |        | v | v | v | v | ... | v |
+/// +--------+---+---+---+---+-----+---+
+/// </pre>
 ///
 /// Here \p element_size would be 2, since to compute the offset it's required
 /// that the vertical size be know. In the above example, with  `n=10`,
@@ -56,14 +57,14 @@ namespace ripple {
 template <typename SpatialImpl, typename... Indices>
 ripple_host_device auto offset_to_soa(
   const MultidimSpace<SpatialImpl>& space       ,
-  std::size_t                       element_size,
+  size_t                            element_size,
   Indices&&...                      indices
-) -> std::size_t {
+) -> size_t {
   constexpr auto num_indices      = sizeof...(Indices);
-  const size_t   ids[num_indices] = {indices...};
-  std::size_t    offset           = ids[0];
+  const size_t   ids[num_indices] = {static_cast<size_t>(indices)...};
+  size_t         offset           = ids[0];
   unrolled_for<num_indices - 1>([&] (auto i) {
-    constexpr auto dim = static_cast<std::size_t>(i) + 1;
+    constexpr auto dim = static_cast<size_t>(i) + 1;
     offset += ids[dim] * element_size * space.step(dim); 
   });
   return offset;
@@ -91,37 +92,38 @@ ripple_host_device auto offset_to_soa(
 /// For the case that the u, v data is allocated as type T, the storage would
 /// look as follows:
 ///
-/// ~~~
-/// index:  |   0   |   1   | ... |   n   |
-///
-/// ---     ==============================|
-///  0      | u | v | u | v | ... | u | v |
-/// ---     ===============================
-///  1      | u | v | u | v | ... | u | v |
-/// ---     ===============================
-///  2      | u | v | u | v | ... | u | v |
-/// ---     ===============================
-/// ~~~
+/// <pre class="diagram">
+/// +--------+-------+-------+-----+-------+
+/// | index: |   0   |   1   | ... |   n   |
+/// +--------+---+---+---+---+-----+---+---+
+/// |   0    | u | v | u | v | ... | u | v |
+/// +--------+---+---+---+---+-----+---+---+
+/// |   1    | u | v | u | v | ... | u | v |
+/// +--------+---+---+---+---+-----+---+---+
+/// |   2    | u | v | u | v | ... | u | v |
+/// +--------+---+---+---+---+-----+---+---+
+/// </pre>
 ///
 /// Here \p element_size would be 2, since to compute the offset it's required
 /// that the horizontal size be know. In the above example, with  `n=10`,
 /// `element_size=2`, `indices=(0, 1)` would return an offset of 20, while 
 /// `indices=(3, 0)` would return an offset of 6.
 ///
-/// For the case that the u, v data is allocated as type Vec<T, 2>, the storage
-/// would look as follows:
+/// For the case that the u, v data is allocated as type 
+/// `Vec<T, 2, contiguous_owned_t>`, the storage would look as follows:
 ///
-/// ~~~
-/// index:  |     0     |     1     | ... |     n      |
+/// <pre class="diagram">
+/// +--------+-----------+-----------+-----+-----------+
+/// | index: |     0     |     1     | ... |      n    |
+/// +--------+-----------+-----------+-----+-----------+
+/// |   0    | Vec<T, 2> | Vec<T, 2> | ... | Vec<T, 2> |
+/// +--------+-----------+-----------+-----+-----------+
+/// |   1    | Vec<T, 2> | Vec<T, 2> | ... | Vec<T, 2> |
+/// +--------+-----------+-----------+-----+-----------+
+/// |   2    | Vec<T, 2> | Vec<T, 2> | ... | Vec<T, 2> |
+/// +--------+-----------+-----------+-----+-----------+
+/// </pre>
 ///
-/// ---     ============================================
-///  0      | Vec<T, 2> | Vec<T, 2> | ... | Vec<>T, 2> |
-/// ---     ============================================
-///  1      | Vec<T, 2> | Vec<T, 2> | ... | Vec<>T, 2> |
-/// ---     ============================================
-///  2      | Vec<T, 2> | Vec<T, 2> | ... | Vec<>T, 2> |
-/// ---     ============================================
-/// ~~~
 ///
 /// Here \p element_size would be 1, since the underlying storage allocated the
 /// complex type contiguously. . In the above example, with  `n=10`,
@@ -136,14 +138,14 @@ ripple_host_device auto offset_to_soa(
 template <typename SpatialImpl, typename... Indices>
 ripple_host_device auto offset_to_aos(
   const MultidimSpace<SpatialImpl>& space       ,
-  std::size_t                       element_size,
+  size_t                            element_size,
   Indices&&...                      indices
-) -> std::size_t {
+) -> size_t {
   constexpr auto num_indices      = sizeof...(Indices);
-  const size_t   ids[num_indices] = {indices...};
+  const size_t   ids[num_indices] = {static_cast<size_t>(indices)...};
   std::size_t    offset           = ids[0] * element_size;
   unrolled_for<num_indices - 1>([&] (auto i) {
-    constexpr auto dim = static_cast<std::size_t>(i) + 1;
+    constexpr auto dim = static_cast<size_t>(i) + 1;
     offset += ids[dim] * element_size * space.step(dim);
   });
   return offset;
