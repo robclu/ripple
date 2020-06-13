cea6ec6c8c9bfc6aaf33442e5bb345539795b520
Rob Clucas <robjclucas@gmail.com>
2020-06-13 16:41:34 +0100

    Update grid tests for graph with dependency

--- tests/graph/graph_tests.hpp
+++ tests/graph/graph_tests.hpp
@@ -18,32 +18,60 @@
 
 #include "node_tests.hpp"
 #include <ripple/core/graph/graph.hpp>
+#include <ripple/core/graph/graph_executor.hpp>
 
-TEST(graph_graph, can_build_graph_node_by_node) {
-  int x = 1;
-  // clang-format off
-  auto graph = ripple::Graph();
-  graph
-    .emplace([&x] { x++; })
-    .emplace([&x] { x++; })
-    .emplace([&x] { x *= 2; });
-  // clang-format on
-
-  EXPECT_EQ(graph.size(), 3);
-}
-
-TEST(graph_graph, can_build_graph_with_make_node) {
+TEST(graph_tests, can_execute_parallel_graph) {
   using namespace ripple;
-  int x = 1;
+  int x = 0, y = 0, z = 0;
   // clang-format off
   auto graph = Graph();
   graph.emplace(
     Graph::make_node([&x] { x++; }),
-    Graph::make_node([&x] { x++; }),
-    Graph::make_node([&x] { x *= 2; }));
+    Graph::make_node([&y] { y++; }),
+    Graph::make_node([&z] { z = 4; }));
   // clang-format on
 
   EXPECT_EQ(graph.size(), 3);
+  graph_executor().execute(graph);
+  graph_executor().wait_until_finished();
+
+  EXPECT_EQ(x, 1);
+  EXPECT_EQ(y, 1);
+  EXPECT_EQ(z, 4);
+}
+
+TEST(graph_tests, can_execute_graph_with_dependency) {
+  using namespace ripple;
+  int x = 0, y = 0, z = 0;
+  // clang-format off
+  auto graph = Graph();
+  graph.emplace(
+    Graph::make_node([&x] { x++; }),
+    Graph::make_node([&y] { y++; }),
+    Graph::make_node([&z] { z = 4; }))
+    .then([&x, &y, &z] {
+      EXPECT_EQ(x, 1);
+      EXPECT_EQ(y, 1);
+      EXPECT_EQ(z, 4);
+      x *= 2;
+      y *= 3;
+      z *= 2;
+    })
+    .then([&x, &y, &z] {
+      EXPECT_EQ(x, 2);
+      EXPECT_EQ(y, 3);
+      EXPECT_EQ(z, 8);
+      x++; y++; z++;
+    });
+  // clang-format on
+
+  EXPECT_EQ(graph.size(), 5);
+  graph_executor().execute(graph);
+  graph_executor().wait_until_finished();
+
+  EXPECT_EQ(x, 3);
+  EXPECT_EQ(y, 4);
+  EXPECT_EQ(z, 9);
 }
 
 #endif // RIPPLE_TESTS_GRAPH_GRAPH_TESTS_HPP
