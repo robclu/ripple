057151974e0e396f910393084228627299c16d7a
Rob Clucas <robjclucas@gmail.com>
2020-09-24 16:25:12 +0100

    Update unrolled_for implementation
    Convert all calls to std::forward<T> to static_cast<T&&> to
    remove the need to include <utility> and slightly improve
    compile time.

--- include/ripple/core/algorithm/detail/unrolled_for_impl_.hpp
+++ include/ripple/core/algorithm/detail/unrolled_for_impl_.hpp
@@ -1,8 +1,8 @@
-//==--- ripple/core/algorithm/detail/unrolled_for_impl_.hpp i---- -*- C++ -*- ---==//
-//            
+//==--- ripple/core/algorithm/detail/unrolled_for_impl_.hpp  -*- C++ -*- ---==//
+//
 //                                Ripple
-// 
-//                      Copyright (c) 2019 Rob Clucas.
+//
+//                      Copyright (c) 2019, 2020 Rob Clucas.
 //
 //  This file is distributed under the MIT License. See LICENSE for details.
 //
@@ -18,74 +18,87 @@
 #define RIPPLE_ALGORITHM_UNROLLED_FOR_IMPL_HPP
 
 #include <ripple/core/utility/number.hpp>
-#include <utility>
 
 namespace ripple {
 namespace detail {
 
-/// The Unroll struct invokes a callable object N times, where the invokations
-/// are unrolled at compile time.
-///
-/// This struct is an implementation detail for `unrolled_for`.
-///
-/// \tparam Amount  The amount of unrolling to do.
-template <std::size_t Amount>
-struct Unroll : Unroll<(Amount <= 1 ? 0 : Amount - 1)> {
-  /// Defines the value for the previous level.
-  static constexpr std::size_t previous_level_v = 
-    (Amount <= 1 ? 0 : Amount - 1);
+/**
+ * The Unroll struct invokes a callable object N times, where the invokations
+ * are unrolled at compile time.
+ *
+ * This struct is an implementation detail for `unrolled_for`.
+ *
+ * \tparam Amount  The amount of unrolling to do.
+ */
+template <size_t Amount>
+struct Unroll : public Unroll<(Amount <= 1 ? 0 : Amount - 1)> {
+  /** Defines the value for the previous level. */
+  static constexpr size_t previous_level = Amount <= 1 ? 0 : Amount - 1;
 
-  /// Defines the type of the case class which invokes at the previous level.
-  using previous_level_t = Unroll<previous_level_v>;
+  /** Defines the type of the case class which invokes at the previous level. */
+  using PreviousLevel = Unroll<previous_level>;
 
-  /// Passes the \p functor and \p args to the previous level to invoke, and
-  /// then invokes at this level.
-  /// \param  functor   The functor to invoke.
-  /// \param  args      The arguments to pass to the functor.
-  /// \tparam Functor   The type of the functor to invoke.
-  /// \tparam Args      The type of the arguments to invoke with.
+  /**
+   * Passes the \p functor and \p args to the previous level to invoke, and
+   * then invokes at this level.
+   * \param  functor   The functor to invoke.
+   * \param  args      The arguments to pass to the functor.
+   * \tparam Functor   The type of the functor to invoke.
+   * \tparam Args      The type of the arguments to invoke with.
+   */
   template <typename Functor, typename... Args>
-  ripple_host_device Unroll(Functor&& functor, Args&&... args)
-  : previous_level_t(
-      std::forward<Functor>(functor), std::forward<Args>(args)... 
-    ) {
-    functor(Num<previous_level_v>(), std::forward<Args>(args)...);
+  ripple_host_device constexpr Unroll(
+    Functor&& functor, Args&&... args) noexcept
+  : PreviousLevel{
+      static_cast<Functor&&>(functor), static_cast<Args&&>(args)...} {
+    functor(Num<previous_level>(), static_cast<Args&&>(args)...);
   }
 };
 
-/// Specialization of the unrolling class to terminate the urolling at the
-/// lowest level.
-///
-/// This struct is an implementation detail for `unrolled_for`.
+/**
+ * Specialization of the unrolling class to terminate the urolling at the
+ * lowest level.
+ *
+ *  This struct is an implementation detail for `unrolled_for`.
+ */
 template <>
 struct Unroll<1> {
-  /// Invokes the functor with the given args.
-  /// \param  functor   The functor to invoke.
-  /// \param  args      The arguments to pass to the functor.
-  /// \tparam Functor   The type of the functor to invoke.
-  /// \tparam Args      The type of the arguments to invoke with.
+  /**
+   * Invokes the functor with the given args.
+   * \param  functor   The functor to invoke.
+   * \param  args      The arguments to pass to the functor.
+   * \tparam Functor   The type of the functor to invoke.
+   * \tparam Args      The type of the arguments to invoke with.
+   */
   template <typename Functor, typename... Args>
-  ripple_host_device Unroll(Functor&& functor, Args&&... args) {
-    functor(Num<0>(), std::forward<Args>(args)...);
+  ripple_host_device constexpr Unroll(
+    Functor&& functor, Args&&... args) noexcept {
+    functor(Num<0>(), static_cast<Args&&>(args)...);
   }
 };
 
-/// Specialization of the unrolling class for the case that 0 unrolling is
-/// specified. This specialization does nothing, it's defined for generic
-/// code that may invoke it.
-///
-/// This struct is an implementation detail for `unrolled_for`.
+/**
+ * Specialization of the unrolling class for the case that 0 unrolling is
+ * specified. This specialization does nothing, it's defined for generic
+ * code that may invoke it.
+ *
+ * This struct is an implementation detail for `unrolled_for`.
+ */
 template <>
 struct Unroll<0> {
-  /// Does nothing.
-  /// \param  functor   Placeholder for a functor.
-  /// \param  args      Placeholder for the functor arguments.
-  /// \tparam Functor   The type of the functor.
-  /// \tparam Args      The type of the arguments.
+  /**
+   * Does nothing.
+   * \param  functor   Placeholder for a functor.
+   * \param  args      Placeholder for the functor arguments.
+   * \tparam Functor   The type of the functor.
+   * \tparam Args      The type of the arguments.
+   */
   template <typename Functor, typename... Args>
-  ripple_host_device constexpr Unroll(Functor&& functor, Args&&... args) {}
+  ripple_host_device constexpr Unroll(
+    Functor&& functor, Args&&... args) noexcept {}
 };
 
-}} // namespace ripple::detail
+} // namespace detail
+} // namespace ripple
 
 #endif // RIPPLE_ALGORITHM_UNROLLED_FOR_HPP
--- include/ripple/core/algorithm/unrolled_for.hpp
+++ include/ripple/core/algorithm/unrolled_for.hpp
@@ -1,7 +1,8 @@
-//==--- ripple/core/algorithm/unrolled_for.hpp ------------------ -*- C++ -*- ---==//
-//            
+//==--- ripple/core/algorithm/unrolled_for.hpp ------------------ -*- C++ -*-
+//---==//
+//
 //                                Ripple
-// 
+//
 //                      Copyright (c) 2019 Ripple.
 //
 //  This file is distributed under the MIT License. See LICENSE for details.
@@ -18,113 +19,110 @@
 #define RIPPLE_ALGORITHM_UNROLLED_FOR_HPP
 
 #include "detail/unrolled_for_impl_.hpp"
-#include <ripple/core/utility/range.hpp>
 
 namespace ripple {
 
-/// Applies the functor Amount times and passes the index of the unrolling
-/// as the first argument to the functor. The unrolling is performed at compile
-/// time, so the number of iterations should not be too large. The index
-/// parameter has a compile-time value, and can therefore be used in constexpr
-/// contexts. For example:
-/// 
-/// ~~~cpp
-/// auto tuple = std::make_tuple("string", 4, AType*());
-/// unrolled_for<2>([&tuple] (auto i) {
-///   do_something(get<i>(tuple), get<i + 1>(tuple)); 
-/// });
-/// ~~~
-/// 
-/// Which effectively will become:
-/// 
-/// ~~~cpp
-/// do_something(get<0>(tuple), get<1>(tuple)); 
-/// do_something(get<1>(tuple), get<2>(tuple)); 
-/// ~~~
-/// 
-/// \note If it's required that the unrolling is bounded,  then use the
-///       `unrolled_for_bounded`.
-///
-/// \param  functor   The functor to unroll.
-/// \param  args      The arguments to the functor.
-/// \tparam Amount    The amount of unrolling to do.
-/// \tparam Functor   The type of the functor.
-/// \tparam Args      The type of the functor arguments.
-template <std::size_t Amount, typename Functor, typename... Args>
-ripple_host_device constexpr inline auto unrolled_for(
-  Functor&& functor, Args&&... args
-) -> void {
+/**
+ * Applies the functor Amount times and passes the index of the unrolling
+ * as the first argument to the functor. The unrolling is performed at compile
+ * time, so the number of iterations should not be too large, otherwise this
+ * will cause code bloat.
+ *
+ * The index parameter has a compile-time value, and can therefore be used in
+ * constexpr contexts. For example:
+ *
+ * ~~~{.cpp}
+ * auto tuple = std::make_tuple("string", 4, AType*());
+ * unrolled_for<2>([&tuple] (auto i) {
+ *   do_something(get<i>(tuple), get<i + 1>(tuple));
+ * });
+ * ~~~
+ *
+ * Which effectively will become:
+ *
+ * ~~~cpp
+ * do_something(get<0>(tuple), get<1>(tuple));
+ * do_something(get<1>(tuple), get<2>(tuple));
+ * ~~~
+ *
+ * \note If it's required that the unrolling is bounded, then use the
+ *       `unrolled_for_bounded`.
+ *
+ * \param  functor   The functor to unroll.
+ * \param  args      The arguments to the functor.
+ * \tparam Amount    The amount of unrolling to do.
+ * \tparam Functor   The type of the functor.
+ * \tparam Args      The type of the functor arguments.
+ */
+template <size_t Amount, typename Functor, typename... Args>
+ripple_host_device constexpr inline auto
+unrolled_for(Functor&& functor, Args&&... args) noexcept -> void {
   detail::Unroll<Amount> unrolled(
-    std::forward<Functor>(functor), std::forward<Args>(args)...
-  );
+    static_cast<Functor&&>(functor), static_cast<Args&&>(args)...);
 }
 
-/// Applies the functor Amount times. However, this is a bounded version and
-/// is safer than `unrolled_for` in that it will not unroll if the value of
-/// Amount is larger than the value defined by `ripple_mac_unroll_depth` at
-/// compile time. 
-///
-/// In the case that Amount is larger than `ripple_max_unroll_length` then
-/// a normal loop is performed and the functor is invoked on each iteration. In
-/// the case that `Amount <= ripple_max_unroll_depth`, then this behaves
-/// exactly as `unrolled_for`. See `unrolled_for` for more details.
-///
-/// This version will not always used a constexpr index, and therefore the index
-/// cannot be used in constexpr contexts.
-///
-/// This overload is only enabled when `Amount < ripple_max_unroll_depth`.
-/// 
-/// \param  functor   The functor to unroll.
-/// \param  args      The arguments to the functor.
-/// \tparam Amount    The amount of unrolling to do.
-/// \tparam Functor   The type of the functor.
-/// \tparam Args      The type of the functor arguments.
+/**
+ * Applies the functor Amount times.
+ *
+ * However, this is a bounded version and is safer than `unrolled_for` in that
+ * it will not unroll if the value of Amount is larger than the value defined by
+ * `ripple_max_unroll_depth` at  compile time.
+ *
+ * In the case that Amount is larger than `ripple_max_unroll_length` then
+ * a normal loop is performed and the functor is invoked on each iteration.
+ *
+ * \note This version will not always use a constexpr index, and therefore the
+ *       index cannot be used in constexpr contexts.
+ *
+ * This overload is only enabled when `Amount < ripple_max_unroll_depth`.
+ *
+ * \param  functor   The functor to unroll.
+ * \param  args      The arguments to the functor.
+ * \tparam Amount    The amount of unrolling to do.
+ * \tparam Functor   The type of the functor.
+ * \tparam Args      The type of the functor arguments.
+ */
 template <
-  std::size_t Amount ,
-  typename    Functor,
-  typename... Args   ,
-  unroll_enabled_t<Amount> = 0
->
-ripple_host_device constexpr inline auto unrolled_for_bounded(
-  Functor&& functor, Args&&... args
-) -> void {
+  size_t Amount,
+  typename Functor,
+  typename... Args,
+  unroll_enabled_t<Amount> = 0>
+ripple_host_device constexpr inline auto
+unrolled_for_bounded(Functor&& functor, Args&&... args) noexcept -> void {
   unrolled_for<Amount>(
-    std::forward<Functor>(functor),
-    std::forward<Args>(args)...   
-  );
+    static_cast<Functor&&>(functor), static_cast<Args&&>(args)...);
 }
 
-/// Applies the functor Amount times. However, this is a bounded version and
-/// is safer than `unrolled_for` in that it will not unroll if the value of
-/// Amount is larger than the value defined by `ripple_mac_unroll_depth` at
-/// compile time. 
-///
-/// In the case that Amount is larger than `ripple_max_unroll_length` then
-/// a normal loop is performed and the functor is invoked on each iteration. In
-/// the case that `Amount <= ripple_max_unroll_depth`, then this behaves
-/// exactly as `unrolled_for`. See `unrolled_for` for more details.
-///
-/// This version will not always used a constexpr index, and therefore the index
-/// cannot be used in constexpr contexts.
-///
-/// This overload is only enabled when `Amount >= ripple_max_unroll_depth`.
-/// 
-/// \param  functor   The functor to unroll.
-/// \param  args      The arguments to the functor.
-/// \tparam Amount    The amount of unrolling to do.
-/// \tparam Functor   The type of the functor.
-/// \tparam Args      The type of the functor arguments.
+/**
+ * Applies the functor Amount times.
+ *
+ * However, this is a bounded version and is safer than `unrolled_for` in that
+ * it will not unroll if the value of Amount is larger than the value defined by
+ * `ripple_max_unroll_depth` at compile time.
+ *
+ * In the case that Amount is larger than `ripple_max_unroll_length` then
+ * a normal loop is performed and the functor is invoked on each iteration.
+ *
+ * This version will not always used a constexpr index, and therefore the index
+ * cannot be used in constexpr contexts.
+ *
+ * This overload is only enabled when `Amount >= ripple_max_unroll_depth`.
+ *
+ * \param  functor   The functor to unroll.
+ * \param  args      The arguments to the functor.
+ * \tparam Amount    The amount of unrolling to do.
+ * \tparam Functor   The type of the functor.
+ * \tparam Args      The type of the functor arguments.
+ */
 template <
-  std::size_t Amount ,
-  typename    Functor,
-  typename... Args   ,
-  unroll_disabled_t<Amount> = 0
->
-ripple_host_device constexpr inline auto unrolled_for_bounded(
-  Functor&& functor, Args&&... args
-) -> void {
-  for (const auto i : range(Amount)) {
-    functor(i, std::forward<Args>(args)...);
+  size_t Amount,
+  typename Functor,
+  typename... Args,
+  unroll_disabled_t<Amount> = 0>
+ripple_host_device constexpr inline auto
+unrolled_for_bounded(Functor&& functor, Args&&... args) noexcept -> void {
+  for (size_t i = 0; i < Amount; ++i) {
+    functor(i, static_cast<Args&&>(args)...);
   }
 }
 
