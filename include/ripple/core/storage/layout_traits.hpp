31d6fff4bd5dd6a808899671ab26fb9d029daff8
Rob Clucas <robjclucas@gmail.com>
2020-09-29 21:02:28 +0100

    Update layout traits

--- include/ripple/core/storage/layout_traits.hpp
+++ include/ripple/core/storage/layout_traits.hpp
@@ -1,8 +1,8 @@
-//==--- ripple/core/storage/layout_traits.hpp ------------------- -*- C++ -*- ---==//
-//            
+//==--- ripple/core/storage/layout_traits.hpp -------------- -*- C++ -*- ---==//
+//
 //                                Ripple
-// 
-//                      Copyright (c) 2019 Rob Clucas.
+//
+//                      Copyright (c) 2019, 2020 Rob Clucas.
 //
 //  This file is distributed under the MIT License. See LICENSE for details.
 //
@@ -23,99 +23,113 @@
 
 namespace ripple {
 
-/// Defines layout traits for the the T. This implementation is for when the
-/// type T owns it's data.
-///
-/// \note This should be used as layout_traits_t<T>, since that performs the
-///       of the specialization condition.
-///
-/// \tparam T                         The type to get the layout traits for.
-/// \tparam StridableAndNonOwningData Condition for specialization.
+/**
+ * Defines layout traits for the template type T. This implementation is for
+ * when the data is owned by the type, other cases are specialized.
+ *
+ * \note This should be used as layout_traits_t<T>, since that aliases the
+ *       correct specializtion.
+ *
+ * \tparam T                         The type to get the layout traits for.
+ * \tparam StridableAndNonOwningData Condition for specialization.
+ */
 template <typename T, bool StridableAndNonOwningData>
 struct LayoutTraits {
-  //==--- [constants] ------------------------------------------------------==//
-
-  /// Defines if the type T is a StridableLayout type.
+  // clang-format off
+  /** Defines if the type T is a StridableLayout type. */
   static constexpr bool is_stridable_layout = false;
-  /// Defines the type of the layout for T.
+  /** Defines the type of the layout for T. */
   static constexpr auto layout_kind         = LayoutKind::none;
-    /// True if the Layout is a LayoutKind::strided_view.
+  /** True if the Layout is a strided view layout kind. */
   static constexpr auto is_strided_view     = false;
 
-  //==--- [traits] ---------------------------------------------------------==//
-
-  /// Defines the value type of T.
-  using value_t          = std::decay_t<T>;
-  /// Defines the type of the allocator for type T.
-  using allocator_t      = typename DefaultStorage<T>::allocator_t;
-  /// Defines the type for making a copy of T.
-  using iter_copy_t      = value_t;
-  /// Defines the type to use when storing in an iterator.
-  using iter_storage_t   = value_t*;
-  /// Defines the type when referencing from an iterator.
-  using iter_ref_t       = value_t&;
-  /// Defines the type of a const reference for an iterator.
-  using iter_const_ref_t = const value_t&;
-  /// Defines the type of a pointer for an iterator.
-  using iter_ptr_t       = value_t*;
-  /// Defines thet type of a const pointer for an iterator.
-  using iter_const_ptr_t = const value_t*;
+  /** Defines the value type of T. */
+  using value_t              = std::decay_t<T>;
+  /** Defines the type of the allocator for type T. */
+  using allocator_t          = typename DefaultStorage<T>::allocator_t;
+  /** Defines the type for making a copy of T. */
+  using iter_copy_t          = value_t;
+  /** Defines the type to use when storing in an iterator. */
+  using iter_storage_t       = value_t*;
+  /** Defines the type when referencing from an iterator. */
+  using iter_ref_t           = value_t&;
+  /** Defines the type of a const reference for an iterator. */
+  using iter_const_ref_t     = const value_t&;
+  /** Defines the type of a pointer for an iterator. */
+  using iter_ptr_t           = value_t*;
+  /** Defines the type of a const pointer for an iterator. */
+  using iter_const_ptr_t     = const value_t*;
+  /** Defines the type of the raw pointer to the data. */
+  using raw_ptr_t            = value_t*;
+  /** Defines the type of the const raw pointer to the data. */
+  using const_raw_ptr_t      = const value_t*;
+  // clang-format on
 };
 
-/// Defines layout traits for the the T when the type implements the
-/// StridableLayout interface, and when the layout kind for the type is a view
-/// type, so does not own its data.
-///
-/// \note This should be used as layout_traits_t<T>, since that performs the
-///       of the specialization condition./
-///
-/// \tparam T The type to get the layout traits for.
+/**
+ * Specialization of the layout traits for caset that the template type T
+ * implements the StridableLayout interface, and when the layout kind for the
+ * type is a view type, which means that it *does not* own the data, and the
+ * data is therefore allocated somewhere else the the storage points to it.
+ *
+ * \note This should be used as layout_traits_t<T>, since that aliases the
+ *       correct specializtion.
+ *
+ * \tparam T The type to get the layout traits for.
+ */
 template <typename T>
 struct LayoutTraits<T, true> {
  private:
-  /// Defines the type of the descriptor for the type T.
+  // clang-format off
+  /** Defines the type of the descriptor for the type T. */
   using descriptor_t      = typename T::descriptor_t;
-  /// Defines the type for strided view storage.
+  /** Defines the type for strided view storage. */
   using strided_view_t    = typename descriptor_t::strided_view_storage_t;
-  /// Defines the type for contiguous view storage.
+  /** Defines the type for contiguous view storage. */
   using contig_view_t     = typename descriptor_t::contig_view_storage_t;
-  /// Defines the type T with contiguous owned storage.
+  /** Defines the type T with contiguous owned storage. */
   using as_contig_owned_t = detail::StorageAs<contiguous_owned_t, T>;
 
  public:
-  //==--- [constants] ------------------------------------------------------==//
+  /*==--- [constants] ------------------------------------------------------==*/
 
-  /// Defines if the type T is a StridableLayout type.
+  /** Defines if the type T is a StridableLayout type. */
   static constexpr bool is_stridable_layout = true;
-  /// Defines the type of the layout for T.
-  static constexpr auto layout_kind         =
-    detail::StorageLayoutKind<T>::value;
-  /// True if the Layout is a LayoutKind::strided_view.
-  static constexpr auto is_strided_view     =
+
+  /** Defines the type of the layout for T. */
+  static constexpr auto layout_kind = detail::StorageLayoutKind<T>::value;
+
+  /** True if the Layout is a strided view kind. */
+  static constexpr auto is_strided_view =
     layout_kind == LayoutKind::strided_view;
-  /// True if the Layout is a LayoutKind::contiguous_view
-  static constexpr auto is_contiguous_view  =
+
+  /** True if the Layout is a contiguous view kind */
+  static constexpr auto is_contiguous_view =
     layout_kind == LayoutKind::contiguous_view;
 
-  //==--- [traits] ---------------------------------------------------------==//
-
-  /// Defines the type to use when storing in an iterator.
-  using iter_storage_t   = 
+  /** Defines the type to use when storing in an iterator. */
+  using iter_storage_t = 
     std::conditional_t<is_strided_view, strided_view_t, contig_view_t>;
-  /// Defines the type of the allocator for type T.
+
+  /** Defines the type of the allocator for type T. */
   using allocator_t      = typename iter_storage_t::allocator_t;
-  /// Defines the value type for the layout.
+  /** Defines the value type for the layout. */
   using value_t          = std::decay_t<T>;
-  /// Defines the type T with owned storage for copying.
+  /** Defines the type T with owned storage for copying. */
   using iter_copy_t      = typename as_contig_owned_t::type;
-  /// Defines the type when referencing from an iterator.
+  /** Defines the type when referencing from an iterator. */
   using iter_ref_t       = value_t;
-  /// Defines the type of a const reference for an iterator.
+  /** Defines the type of a const reference for an iterator. */
   using iter_const_ref_t = const value_t;
-  /// Defines the type of a pointer for an iterator.
+  /** Defines the type of a pointer for an iterator. */
   using iter_ptr_t       = PointerWrapper<value_t>;
-  /// Defines thet type of a const pointer for an iterator.
+  /** Defines thet type of a const pointer for an iterator. */
   using iter_const_ptr_t = const iter_ptr_t;
+  /** Defines the type of the raw pointer to the data. */
+  using raw_ptr_t        = void*;
+  /** Defines the type of the const raw pointer to the data. */
+  using const_raw_ptr_t  = const void*;
+  // clang-format off
 };
 
 } // namespace ripple
