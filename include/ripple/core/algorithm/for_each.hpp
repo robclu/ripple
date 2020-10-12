88479e58c220eb5301fa31ef593a2489c94513d8
Rob Clucas <robjclucas@gmail.com>
2020-10-12 11:49:42 +0100

    Refactor for_each

--- include/ripple/core/algorithm/for_each.hpp
+++ include/ripple/core/algorithm/for_each.hpp
@@ -1,8 +1,8 @@
 //==--- ripple/core/algorithm/for_each.hpp ----------------- -*- C++ -*- ---==//
-//            
+//
 //                                Ripple
-// 
-//                      Copyright (c) 2020 Rob Clucas
+//
+//                      Copyright (c) 2019, 2020 Rob Clucas
 //
 //  This file is distributed under the MIT License. See LICENSE for details.
 //
@@ -21,78 +21,94 @@
 
 namespace ripple {
 
-//==--- [tuple] ------------------------------------------------------------==//
+/*==--- [tuple] ------------------------------------------------------------==*/
 
-/// Applies the \p functor to each of the elements in the \p tuple, when the
-/// tuple is a non-const reference.
-///
-/// \param  tuple     The tuple to apply the functor to.
-/// \param  functor   The functor to apply to each of the arguments.
-/// \param  func_args Additional arguments for the functor.
-/// \tparam Functor   The type of the functor.
-/// \tparam Args      The type of the tuple arguments.
-/// \tparam FuncArgs  Additional arguments for the functor.
+/**
+ * Applies the functor to each of the elements in the tuple.
+ *
+ * \note This overload is for a non-const lvalue reference tuple.
+ *
+ * \param  tuple     The tuple to apply the functor to.
+ * \param  functor   The functor to apply to each of the arguments.
+ * \param  func_args Additional arguments for the functor.
+ * \tparam Functor   The type of the functor.
+ * \tparam Args      The type of the tuple arguments.
+ * \tparam FuncArgs  Additional arguments for the functor.
+ */
 template <typename Functor, typename... Args, typename... FuncArgs>
 ripple_host_device constexpr auto for_each(
-  Tuple<Args...>& tuple, Functor&& functor, FuncArgs&&... func_args
-) -> void {
-  constexpr auto num_args = sizeof...(Args);
-  unrolled_for<num_args>([&] ripple_host_device (auto i) -> void {
-    functor(get<i>(tuple), std::forward<FuncArgs>(func_args)...);
+  Tuple<Args...>& tuple, Functor&& functor, FuncArgs&&... func_args) noexcept
+  -> void {
+  constexpr size_t num_args = sizeof...(Args);
+  unrolled_for<num_args>([&] ripple_host_device(auto i) -> void {
+    functor(get<i>(tuple), static_cast<FuncArgs&&>(func_args)...);
   });
 }
 
-/// Applies the \p functor to each of the elements in the \p tuple, when the
-/// tuple is a const reference.
-///
-/// \param  tuple     The tuple to apply the functor to.
-/// \param  functor   The functor to apply to each of the arguments.
-/// \param  func_args Additional arguments for the functor.
-/// \tparam Functor   The type of the functor.
-/// \tparam Args      The type of the tuple arguments.
-/// \tparam FuncArgs  Additional arguments for the functor.
+/**
+ * Applies the functor to each of the elements in the tuple.
+ *
+ * \note This overload is for a a const reference tuple.
+ *
+ * \param  tuple     The tuple to apply the functor to.
+ * \param  functor   The functor to apply to each of the arguments.
+ * \param  func_args Additional arguments for the functor.
+ * \tparam Functor   The type of the functor.
+ * \tparam Args      The type of the tuple arguments.
+ * \tparam FuncArgs  Additional arguments for the functor.
+ */
 template <typename Functor, typename... Args, typename... FuncArgs>
 ripple_host_device constexpr auto for_each(
-  const Tuple<Args...>& tuple, Functor&& functor, FuncArgs&&... func_args
-) -> void {
-  constexpr auto num_args = sizeof...(Args);
-  unrolled_for<num_args>([&] ripple_host_device (auto i) -> void {
-    functor(get<i>(tuple), std::forward<FuncArgs>(func_args)...);
+  const Tuple<Args...>& tuple,
+  Functor&&             functor,
+  FuncArgs&&... func_args) noexcept -> void {
+  constexpr size_t num_args = sizeof...(Args);
+  unrolled_for<num_args>([&] ripple_host_device(auto i) -> void {
+    functor(get<i>(tuple), static_cast<FuncArgs&&>(func_args)...);
   });
 }
 
-/// Applies the \p functor to each of the elements in the \p tuple, when the
-/// tuple is a const reference.
-///
-/// \param  tuple     The tuple to apply the functor to.
-/// \param  functor   The functor to apply to each of the arguments.
-/// \param  func_args Additional arguments for the functor.
-/// \tparam Functor   The type of the functor.
-/// \tparam Args      The type of the tuple arguments.
-/// \tparam FuncArgs  Additional arguments for the functor.
+/**
+ * Applies the functor to each of the elements in the tuple.
+ *
+ * \note This overload is for an rvalue reference tuple.
+ *
+ * \param  tuple     The tuple to apply the functor to.
+ * \param  functor   The functor to apply to each of the arguments.
+ * \param  func_args Additional arguments for the functor.
+ * \tparam Functor   The type of the functor.
+ * \tparam Args      The type of the tuple arguments.
+ * \tparam FuncArgs  Additional arguments for the functor.
+ */
 template <typename Functor, typename... Args, typename... FuncArgs>
 ripple_host_device constexpr auto for_each(
-  Tuple<Args...>&& tuple, Functor&& functor, FuncArgs&&... func_args
-) -> void {
-  constexpr auto num_args = sizeof...(Args);
-  unrolled_for<num_args>([&] ripple_host_device (auto i) -> void {
-    functor(std::move(get<i>(tuple)), std::forward<FuncArgs>(func_args)...);
+  Tuple<Args...>&& tuple, Functor&& functor, FuncArgs&&... func_args) noexcept
+  -> void {
+  constexpr size_t num_args = sizeof...(Args);
+  unrolled_for<num_args>([&] ripple_host_device(auto i) -> void {
+    using MoveType = std::remove_reference_t<decltype(get<i>(tuple))>;
+    functor(
+      static_cast<MoveType&&>(get<i>(tuple)),
+      static_cast<FuncArgs&&>(func_args)...);
   });
 }
 
-//==--- [parameter pack] ---------------------------------------------------==//
+/*==--- [parameter pack] ---------------------------------------------------==*/
 
-/// Applies the \p functor to each of the \p args.
-/// \param  functor   The functor to apply to each of the arguments.
-/// \param  args      The arguments to apply the functor to.
-/// \tparam Functor   The type of the functor.
-/// \tparam Args      The type of the arguments.
+/**
+ * Applies the functor to each of the args.
+ *
+ * \param  functor   The functor to apply to each of the arguments.
+ * \param  args      The arguments to apply the functor to.
+ * \tparam Functor   The type of the functor.
+ * \tparam Args      The type of the arguments.
+ */
 template <typename Functor, typename... Args>
-ripple_host_device constexpr auto for_each(Functor&& functor, Args&&... args)
--> void {
+ripple_host_device constexpr auto
+for_each(Functor&& functor, Args&&... args) -> void {
+  using TupleType = Tuple<static_cast<Args&&>...>;
   for_each(
-    make_tuple(std::forward<Args>(args)...), std::forward<Functor>(functor)
-  );
+    Tuple{static_cast<Args&&>(args)...}, static_cast<Functor&&>(functor));
 }
 
 } // namespace ripple
