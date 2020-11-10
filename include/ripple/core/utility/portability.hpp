6254410bb46eca224e590df26ae34a86c28a5a7b
robclu <robjclucas@gmail.com>
2020-11-10 16:33:18 +0000

    Update portability for clang with sm_80,sm_86

--- include/ripple/core/utility/portability.hpp
+++ include/ripple/core/utility/portability.hpp
@@ -1,9 +1,8 @@
-//==--- ripple/core/utility/portability.hpp --------------------- -*- C++ -*-
-//---==//
+//==--- ripple/core/utility/portability.hpp ---------------- -*- C++ -*- ---==//
 //
 //                                Ripple
 //
-//                      Copyright (c) 2019 Rob Clucas.
+//                      Copyright (c) 2019, 2020 Rob Clucas.
 //
 //  This file is distributed under the MIT License. See LICENSE for details.
 //
@@ -20,72 +19,93 @@
 #include <cstddef>
 #include <cstdio>
 #include <cuda.h>
+#include <cuda_runtime_api.h>
 #include <cuda_runtime.h>
 #include <exception>
-#include <cooperative_groups.h>
+
+/**
+ * Clang currently has a problem including the crt device function headers for
+ * sm80 and sm 86, so they have to be manually included for now.
+ */
+#if defined(__clang__) && defined(RIPPLE_SM_80)
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-W#warnings"
+  #include <crt/sm_80_rt.h>
+  #pragma clang diagnostic pop
+#endif
 
 // clang-format off
-/// Definitions for host, device, and host device functions
-/// if CUDA is supported.
+/**
+ * Definitions for host, device, and host device functions
+ * if CUDA is supported by either nvcc or clang.
+ */
 #if defined(__CUDACC__) || defined(__clang__)
-  /// Defines if GPU functionality is available.
+  /** Defines if GPU functionality is available. */
   #define ripple_gpu_available  true
-  /// Defines if CUDA functionality is available
+  /** Defines if CUDA functionality is available */
   #define ripple_cuda_available true
-  /// Defines a speficier for a host only function.
-  #define ripple_host_only      __host__
-  /// Defines a specifier for a device only function.
-  #define ripple_device_only    __device__
-  /// Defines a speficier for a host and device function.
+  /** Defines a speficier for a host only function. */
+  #define ripple_host           __host__
+  /** Defines a specifier for a device only function. */
+  #define ripple_device         __device__
+  /** Defines a speficier for a host and device function. */
   #define ripple_host_device    __host__ __device__
-  /// Defines a specifier for a global kernel function.
+  /** Defines a specifier for a global kernel function. */
   #define ripple_global         __global__
-
-  /// Macro for thread synchronization for the device.
-  #define ripple_syncthreads() __syncthreads()
+  /* Defines valid code if cuda is available. */
+  #define ripple_if_cuda(...)   __VA_ARGS__
 #else
-  /// Defines if GPU functionality is available.
+  /** Defines if GPU functionality is available. */
   #define ripple_gpu_available  false
-  /// Defines if CUDA functionality is available
+  /** Defines if CUDA functionality is available */
   #define ripple_cuda_available false
-  /// Defines a speficier for a host only function.
-  #define ripple_host_only
-  /// Defines a specifier for a device only function.
-  #define ripple_device_only
-  /// Defines a speficier for a host and device function.
+  /** Defines a speficier for a host only function. */
+  #define ripple_host
+  /** Defines a specifier for a device only function. */
+  #define ripple_device
+  /** Defines a speficier for a host and device function. */
   #define ripple_host_device
-  /// Defines a specifier for a global kernel function.
+  /** Defines a specifier for a global kernel function. */
   #define ripple_global
-
-  /// Macro for thread synchronization for the host.
-  #define ripple_syncthreads() 
+  /** Removes the code defined in the argument. */
+  #define ripple_if_cuda(...) 
 #endif
 // clang-format on
 
-//==--- [compiler] ---------------------------------------------------------==//
+/*==--- [compiler] ---------------------------------------------------------==*/
 
-/// Defines if clang is being used.
+/** Defines if clang is being used. */
 #define ripple_clang __clang__
-/// Defines if gcc is being used.
+/** Defines if gcc is being used. */
 #define riplple_gcc __GNUC__ && !(__clang__) && !(__CUDACC__)
-/// Defines if nvcc is being used.
+/** Defines if nvcc is being used. */
 #define ripple_nvcc __CUDACC__ && !(__clang__)
 
-#ifndef MAX_UNROLL_DEPTH
-  /// Defines the max depth for compile time unrolling.
+#ifndef RIPPLE_MAX_UNROLL_DEPTH
+  /** Defines the max depth for compile time unrolling. */
   #define ripple_max_unroll_depth 8
 #else
-  /// Defines the max depth for compile time unrolling.
-  #define ripple_max_unroll_depth MAX_UNROLL_DEPTH
+  /** Defines the max depth for compile time unrolling. */
+  #define ripple_max_unroll_depth RIPPLE_MAX_UNROLL_DEPTH
+#endif
+
+#if __cplusplus >= 201703L
+  /** Macro for nodiscard */
+  #define ripple_nodiscard [[nodiscard]]
+#else
+  /** Macro for nodiscard */
+  #define ripple_nodiscard
 #endif
 
 namespace ripple::cuda::debug {
 
-/// Checks if a cuda error code was a success, and if not, prints the error
-/// message.
-/// \param[in] err_code The cuda error code.
-/// \param[in] file     The file where the error was detected.
-/// \param[in] line     The line in the file where the error was detected.
+/**
+ * Checks if a cuda error code was a success, and if not, prints the error
+ * message.
+ * \param err_code The cuda error code.
+ * \param file     The file where the error was detected.
+ * \param line     The line in the file where the error was detected.
+ */
 inline auto
 check_cuda_error(cudaError_t err_code, const char* file, int line) -> void {
   if (err_code != cudaSuccess) {
@@ -101,13 +121,16 @@ check_cuda_error(cudaError_t err_code, const char* file, int line) -> void {
 } // namespace ripple::cuda::debug
 
 #if defined(NDEBUG)
-  /// Defines a macro for checking a cuda error in release mode. This does not
-  /// do anything so that there is no performance cost in release mode.
+  /**
+   * Defines a macro for checking a cuda error in release mode. This does not
+   * do anything so that there is no performance cost in release mode.
+   */
   #define ripple_check_cuda_result(result) (result)
 #else
-  /// Defines a macro to check the result of cuda calls in debug mode.
+  /** Defines a macro to check the result of cuda calls in debug mode. */
   #define ripple_check_cuda_result(result) \
-    ::ripple::cuda::debug::check_cuda_error((result), __FILE__, __LINE__)
+    ripple_if_cuda(                        \
+      ::ripple::cuda::debug::check_cuda_error((result), __FILE__, __LINE__))
 #endif // NDEBUG
 
 #endif // RIPPLE_UTILITY_PORTABILITY_HPP
