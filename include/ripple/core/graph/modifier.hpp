30f3cde4f8a59352f7bbe3dde3fe058ed6ea6e74
robclu <robjclucas@gmail.com>
2020-12-11 21:22:41 +0000

    Add expansion and overlap modifiers

--- include/ripple/core/graph/modifier.hpp
+++ include/ripple/core/graph/modifier.hpp
@@ -18,8 +18,6 @@
 #define RIPPLE_GRAPH_MODIFIER_HPP
 
 #include <ripple/core/utility/type_traits.hpp>
-#include <ripple/core/container/block_traits.hpp>
-#include <ripple/core/container/tensor.hpp>
 
 namespace ripple {
 
@@ -27,11 +25,17 @@ namespace ripple {
  * Defines the type of the modifier.
  */
 enum class Modifier : uint8_t {
-  concurrent        = 0, //!< Concurrent modification.
-  exclusive         = 1, //!< Exclusive modification.
-  shared            = 2, //!< Uses shared memory.
-  concurrent_shared = 3, //!< Concurrent and uses shared memory.
-  exclusive_shared  = 4  //!< Exclusive and uses shared memory.
+  concurrent                 = 0,  //!< Concurrent modification.
+  exclusive                  = 1,  //!< Exclusive modification.
+  shared                     = 2,  //!< Uses shared memory.
+  concurrent_shared          = 3,  //!< Concurrent and uses shared memory.
+  exclusive_shared           = 4,  //!< Exclusive and uses shared memory.
+  expander                   = 5,  //!< Expands into padding.
+  concurrent_expander        = 6,  //!< Concurrent & expander
+  exclusive_expander         = 7,  //!< Exclusive & expander
+  shared_expander            = 8,  //!< Shared & expander.
+  concurrent_shared_expander = 9,  //!< Concurrent & shared & expander.
+  exclusive_shared_expander  = 10, //!< Exclusive & shared  & expander
 };
 
 /**
@@ -43,7 +47,9 @@ enum class Modifier : uint8_t {
  */
 template <typename T, Modifier Modification = Modifier::concurrent>
 struct ModificationSpecifier {
-  T wrapped; //!< The type being wrapped.
+  T   wrapped;       //!< The type being wrapped.
+  int expansion = 0; //!< The amount to expand into the padding.
+  int overlap   = 0; //!< The amount of overlap.
 };
 
 /*==--- [traits] -----------------------------------------------------------==*/
@@ -68,6 +74,8 @@ struct ModificationTraits {
   static constexpr bool uses_shared             = false;
   /** Returns true if the modifier is exclusive or concurrent. */
   static constexpr bool exclusive_or_concurrent = false;
+  /** Returns true if the modifier is an expander. */
+  static constexpr bool is_expander             = false;
   // clang-format on
 };
 
@@ -87,22 +95,40 @@ struct ModificationTraits<ModificationSpecifier<T, Modification>> {
 
   /** Returns true if the modification is concurrent. */
   static constexpr bool is_concurrent = 
-    Modification == Modifier::concurrent ||
-    Modification == Modifier::concurrent_shared;
+    Modification == Modifier::concurrent          ||
+    Modification == Modifier::concurrent_shared   ||
+    Modification == Modifier::concurrent_expander ||
+    Modification == Modifier::concurrent_shared_expander;
 
   /** Returns true if the modification is exclusive. */
   static constexpr bool is_exclusive = 
-    Modification == Modifier::exclusive ||
-    Modification == Modifier::exclusive_shared;
+    Modification == Modifier::exclusive          ||
+    Modification == Modifier::exclusive_shared   ||
+    Modification == Modifier::exclusive_expander ||
+    Modification == Modifier::exclusive_shared_expander;
 
   /** Returns true if the modification requires shared memory. */
   static constexpr bool uses_shared =
-    Modification == Modifier::shared           ||
-    Modification == Modifier::exclusive_shared ||
-    Modification == Modifier::concurrent_shared;
+    Modification == Modifier::shared                    ||
+    Modification == Modifier::exclusive_shared          ||
+    Modification == Modifier::concurrent_shared         ||
+    Modification == Modifier::shared_expander           ||
+    Modification == Modifier::exclusive_shared_expander ||
+    Modification == Modifier::concurrent_shared_expander;
+
+  /** Returns true if this is an expander modifier. */
+  static constexpr bool is_expander   = 
+    Modification == Modifier::expander                  ||
+    Modification == Modifier::shared_expander           ||
+    Modification == Modifier::concurrent_expander       ||
+    Modification == Modifier::exclusive_expander        ||
+    Modification == Modifier::exclusive_shared_expander ||
+    Modification == Modifier::concurrent_shared_expander;
+
 
   /** Returns true if the modification is exclusive or concurrent. */
   static constexpr bool exclusive_or_concurrent = is_exclusive || is_concurrent;
+
   // clang-format on
 };
 
@@ -119,6 +145,14 @@ using modification_traits_t = ModificationTraits<std::decay_t<T>>;
  */
 template <typename T>
 static constexpr bool is_modifier_v = modification_traits_t<T>::is_modifier;
+
+/**
+ * Determines if the given type is an expander modifier.
+ * \tparam T The type to determine if is an expander.
+ */
+template <typename T>
+static constexpr bool is_expander_modifier_v =
+  modification_traits_t<T>::is_expander;
 
 /**
  * Returns true if any of the template parameters have either exclusive or
@@ -147,7 +181,47 @@ template <typename T>
 using non_shared_mod_enable_t =
   std::enable_if_t<!modification_traits_t<T>::uses_shared, int>;
 
+/**
+ * Defines a valid type if it is a modifier.
+ * \tparam T The type to base the enable on.
+ */
+template <typename T>
+using modifier_enable_t = std::enable_if_t<is_modifier_v<T>, int>;
+
+/**
+ * Defines a valid type if T is not a modifier.
+ * \tparam T The type to base the enable on.
+ */
+template <typename T>
+using non_modifier_enable_t = std::enable_if_t<!is_modifier_v<T>, int>;
+
 /*==--- [methods] ----------------------------------------------------------==*/
+
+/**
+ * Parameters specifying the type of expansion.
+ */
+struct ExpansionParams {
+  int expansion = 0; //!< The amount of expansion into padding.
+  int overlap   = 0; //!< The amount of overlapping cells.
+};
+
+/**
+ * Creates expansion parameters for the given expansion.
+ * \param The amount of expansion on each side of each dimension.
+ * \return The expansion paramters defining the expansion.
+ */
+auto with_expansion(int expansion) noexcept -> ExpansionParams {
+  return ExpansionParams{expansion, 0};
+}
+
+/**
+ * Creates expansion parameters for the given overlap.
+ * \param The amount of overlap for each block of execution.
+ * \return The expansion paramters defining the overlap.
+ */
+auto with_overlap(int overlap) noexcept -> ExpansionParams {
+  return ExpansionParams{overlap, overlap};
+}
 
 /**
  * Creates a wrapped type as a concurrent modifier which requies padding.
@@ -158,6 +232,18 @@ using non_shared_mod_enable_t =
 template <typename T, typename Type = std::remove_reference_t<T>>
 auto concurrent_padded_access(T& t) noexcept {
   return ModificationSpecifier<Type&, Modifier::concurrent>{t};
+}
+
+/**
+ * Creates a wrapped type as a concurrent modifier which requies padding.
+ * \param  t The instance to wrap.
+ * \tparam T The type to wrap in a modifier.
+ * \return The type wrapped in a modifier.
+ */
+template <typename T, typename Type = std::remove_reference_t<T>>
+auto concurrent_padded_access(T& t, ExpansionParams params) noexcept {
+  return ModificationSpecifier<Type&, Modifier::concurrent_expander>{
+    t, params.expansion, params.overlap};
 }
 
 /**
@@ -172,10 +258,24 @@ auto exclusive_padded_access(T& t) noexcept {
 }
 
 /**
- * Creates a wrapped type as a concurrent modififer which uses shared memory and
- * which requires padding.
+ * Creates a wrapped type as an exclusive modifier which required padding.
  * \param  t The instance to wrap.
  * \tparam T The type to wrap in a modifier.
+ * \return The type wrapped in a modifier.
+ */
+template <typename T, typename Type = std::remove_reference_t<T>>
+auto exclusive_padded_access(T& t, ExpansionParams params) noexcept {
+  return ModificationSpecifier<Type&, Modifier::exclusive_expander>{
+    t, params.expansion, params.overlap};
+}
+
+/**
+ * Creates a wrapped type as a concurrent modififer which uses shared memory and
+ * which requires padding.
+ * \param  t         The instance to wrap.
+ * \param  expantion Number of cells to expand the domain size by, reducing the
+ *                   padding by this amount on each size of each dimension.
+ * \tparam T         The type to wrap in a modifier.
  * \return The type wrapped in the modifier.
  */
 template <typename T, typename Type = std::remove_reference_t<T>>
@@ -184,15 +284,47 @@ auto concurrent_padded_access_in_shared(T& t) noexcept {
 }
 
 /**
+ * Creates a wrapped type as a concurrent modififer which uses shared memory and
+ * which requires padding.
+ * \param  t         The instance to wrap.
+ * \param  expantion Number of cells to expand the domain size by, reducing the
+ *                   padding by this amount on each size of each dimension.
+ * \tparam T         The type to wrap in a modifier.
+ * \return The type wrapped in the modifier.
+ */
+template <typename T, typename Type = std::remove_reference_t<T>>
+auto concurrent_padded_access_in_shared(T& t, ExpansionParams params) noexcept {
+  return ModificationSpecifier<Type&, Modifier::concurrent_shared_expander>{
+    t, params.expansion, params.overlap};
+}
+
+/**
  * Creates a wrapped type as an exclusive modifier which uses shared memory and
  * which requires padding.
- * \param  t The instance to wrap.
- * \tparam T The type to wrap in a modifier.
+ * \param  t         The instance to wrap.
+ * \param  expantion Number of cells to expand the domain size by, reducing the
+ *                   padding by this amount on each size of each dimension.
+ * \tparam T         The type to wrap in a modifier.
  * \return The type wrapped in a modifier.
  */
 template <typename T, typename Type = std::remove_reference_t<T>>
 auto exclusive_padded_access_in_shared(T& t) noexcept {
   return ModificationSpecifier<Type&, Modifier::exclusive_shared>{t};
+}
+
+/**
+ * Creates a wrapped type as an exclusive modifier which uses shared memory and
+ * which requires padding.
+ * \param  t         The instance to wrap.
+ * \param  expantion Number of cells to expand the domain size by, reducing the
+ *                   padding by this amount on each size of each dimension.
+ * \tparam T         The type to wrap in a modifier.
+ * \return The type wrapped in a modifier.
+ */
+template <typename T, typename Type = std::remove_reference_t<T>>
+auto exclusive_padded_access_in_shared(T& t, ExpansionParams params) noexcept {
+  return ModificationSpecifier<Type&, Modifier::exclusive_shared_expander>{
+    t, params.expansion, params.overlap};
 }
 
 /**
@@ -204,6 +336,34 @@ auto exclusive_padded_access_in_shared(T& t) noexcept {
 template <typename T, typename Type = std::remove_reference_t<T>>
 auto in_shared(T& t) noexcept {
   return ModificationSpecifier<Type&, Modifier::shared>{t};
+}
+
+/**
+ * Creates a wrapped type as a modifier which expands into the padding for
+ * the data which is wrapper.
+ * \param  t         The instance to wrap.
+ * \param  expantion Number of cells to expand the domain size by, reducing the
+ *                   padding by this amount on each size of each dimension.
+ * \tparam T         The type of the instance.
+ * \return The type wrapped in a modifier.
+ */
+template <typename T, typename Type = std::remove_reference_t<T>>
+auto expanded(T& t, int expansion) noexcept {
+  return ModificationSpecifier<Type&, Modifier::expander>{t, expansion};
+}
+
+/**
+ * Creates a wrapped type as a modifier which expands into the padding for
+ * the data which is wrapper.
+ * \param  t         The instance to wrap.
+ * \param  expantion Number of cells to expand the domain size by, reducing the
+ *                   padding by this amount on each size of each dimension.
+ * \tparam T         The type of the instance.
+ * \return The type wrapped in a modifier.
+ */
+template <typename T, typename Type = std::remove_reference_t<T>>
+auto overlapped(T& t, int overlap) noexcept {
+  return ModificationSpecifier<Type&, Modifier::expander>{t, overlap, overlap};
 }
 
 /**
@@ -234,6 +394,20 @@ template <typename T, Modifier M>
 auto unwrap_modifiers(ModificationSpecifier<T, M> t) noexcept
   -> std::remove_reference_t<decltype(t.wrapped)>& {
   return t.wrapped;
+}
+
+/**
+ * Gets the expansion paramters if the type is a modifier.
+ * \tparam T The type to unwrap.
+ * \return The expansion parameters representing the expansion.
+ */
+template <typename T>
+auto get_modifier_expansion_params(const T& t) noexcept -> ExpansionParams {
+  if constexpr (is_modifier_v<T>) {
+    return ExpansionParams{t.expansion, t.overlap};
+  } else {
+    return ExpansionParams{};
+  }
 }
 
 } // namespace ripple
